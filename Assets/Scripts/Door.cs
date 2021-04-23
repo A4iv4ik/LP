5759514edb110cedbf08fcc532c7ae63340fa03e
A4iv4ik <danya.matyunin.90@mail.ru>
2021-04-23 20:51:17 +0400

    Initial commit

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+	private KeyCode key = KeyCode.F;
+	[SerializeField]private AudioSource open;
+	[SerializeField]private AudioSource close;
+	[SerializeField]private Transform player;
+	[SerializeField] private Transform anchor; 
+	[SerializeField] private float distance = 2f; 
+	[SerializeField] private bool isOpen = false; 
+	[SerializeField] private float openAngle = 120f;
+	[SerializeField] private float closeAngle = 0f;
+	[SerializeField] private float smooth = 2f;
+
+	void Awake()
+	{
+		isOpen = false;
+		
+	}
+	
+	void Update()
+	{
+		if (Vector3.Distance(transform.position, player.position) < distance && Input.GetKeyDown(key))
+		{
+            if (isOpen)
+            {
+				isOpen = false;
+				open.Play();
+			
+            }
+            else
+            {
+			    isOpen = true;
+				close.Play();
+            }
+		}
+			if (isOpen)
+			{
+
+				Quaternion rotation = Quaternion.Euler(0, openAngle, 0);
+				anchor.localRotation = Quaternion.Lerp(anchor.localRotation, rotation, smooth * Time.deltaTime);
+			}
+			else
+			{
+				Quaternion rotation = Quaternion.Euler(0, closeAngle, 0);
+				anchor.localRotation = Quaternion.Lerp(anchor.localRotation, rotation, smooth * Time.deltaTime);
+			}
+
+			
+		
+	}
+
+	
+}
---
+++ Assets/Scripts/move.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    [SerializeField] private float speed = 2f;
+    [SerializeField] private float _MouseSensetive;
+    [SerializeField] private Transform _camera;
+
+    Vector3 _direction = Vector3.zero;
+    float _angle;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+      
+        _direction.x = Input.GetAxis("Horizontal");
+        _direction.z = Input.GetAxis("Vertical");
+        _angle = Input.GetAxis("Mouse X");
+        Camerarotator();
+    }
+    private void FixedUpdate()
+    {
+        Move();
+    }
+    private void Camerarotator()
+    {
+        _camera.transform.Rotate(new Vector3(0f, _angle*_MouseSensetive*Time.fixedDeltaTime, 0f));
+    }
+    private void Move()
+    {
+        var _speed = _direction * Time.fixedDeltaTime * speed;
+        
+        player.transform.Translate(_speed);
+        //
+        //if (_speed!=Vector3.zero)
+        //{
+        //player.transform.forward = _speed;
+        //    
+        //}
+
+    }
+}
